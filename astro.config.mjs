b08a53a1d4ced8102a2848d6d570ff50bc630840
Paul Klink <paul@klink.au>
2024-11-29 17:07:12 +1100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Set the vite scss api property below if the following warning is displayed:
+    // Deprecation Warning: The legacy JS API is deprecated and will be removed in Dart Sass 2.0.0.
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler' // or "modern"
+                }
+            }
+        }
+    }
+});
---
+++ src/pages/index.md
@@ -0,0 +1,10 @@
+---
+layout: ../layouts/Base.astro
+title: 'pbkware software'
+---
+
+# pbkware software
+
+Note all software available from this website is provided as is, with no warranty what so ever.
+
+1. [Astro Main Menu](./astro-main-menu)
